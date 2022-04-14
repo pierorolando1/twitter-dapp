a6f104276fdaccac730a74c1408c286e9ab668da
Piero Rolando <rolandopiero63@gmail.com>
2022-04-14 13:35:48 -0400

    Setup NextUi, and init Anchor project

---
+++ src/pages/_document.tsx
@@ -0,0 +1,25 @@
+import Document, { Html, Head, Main, NextScript, DocumentContext, DocumentInitialProps } from 'next/document';
+import { CssBaseline } from '@nextui-org/react';
+
+export default class MyDocument extends Document {
+    static async getInitialProps(ctx: DocumentContext): Promise<DocumentInitialProps> {
+        const initialProps = await Document.getInitialProps(ctx);
+        return {
+            ...initialProps,
+            styles: <>{initialProps.styles}</> as any,
+        };
+    }
+
+    render() {
+        return (
+            <Html lang="en">
+                <Head>{CssBaseline.flush()}</Head>
+                <body>
+                    <Main />
+                    <NextScript />
+                </body>
+            </Html>
+        );
+    }
+}
+
